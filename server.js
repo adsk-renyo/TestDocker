f53e76e3e02e5b4acbc6c6ce0cfcc199475b244d
renyongfu <adsk.renyo@gmail.com>
2016-11-16 14:31:19 -0500

    add mongo db for nodejs

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const express = require('express');
-
+const mongoose = require('mongoose')
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
+mongoose.connect("mongodb://mongo:27017");
+
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  res.send('Hello world with mongo\n');
 });
 
 app.listen(PORT, function() {
